050e065e59bbe9018f2846c7f45525c3e5a9d596
k2mac55user <08-augment-lamb@icloud.com>
2021-10-21 14:20:05 -0700

    Update to add the files again

---
+++ business-card/lambda/lambda_image_function.py
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+var s3 = new AWS.S3();
+exports.handler = (event, context, callback) => {
+     let encodedImage =JSON.parse(event.body).user_avatar;
+     let decodedImage = Buffer.from(encodedImage, 'base64');
+     var filePath = "profile_photo/" + event.queryStringParameters.name + ".jpg"
+     var params = {
+       "Body": decodedImage,
+       "Bucket": "cfs3-webapps-business-card-profile-photo-bucket",
+       "Key": filePath  
+    };
+    s3.upload(params, function(err, data){
+       if(err) {
+           callback(err, null);
+       } else {
+           let response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+    };
+           callback(null, response);
+    }
+    });
+    
+};
